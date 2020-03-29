0df5a6eb726d2ec9441262b5ebc862a702f763ff
RiriPhoenix <58237630+RiriPhoenix@users.noreply.github.com>
2020-03-29 13:21:42 -0300

    Add files via upload

---
+++ Nave.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nave : MonoBehaviour
+{
+    public float torque = 1f;
+    public float force = 5f;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.AddTorque ((force * torque) * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.AddTorque ((-force * torque) * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddRelativeForce(Vector3.up * (-force / 100), ForceMode2D.Impulse);
+        }
+
+    }
+}
